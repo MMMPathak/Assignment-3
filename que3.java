716404bdd75a55755c703fa0e4dc0a4418080673
Mrunmayi Pathak <102546556+MMMPathak@users.noreply.github.com>
2024-03-15 12:06:26 +0530

    Add files via upload

---
+++ que3.java
@@ -0,0 +1,23 @@
+package Typecastingcon;
+
+public class que3 {
+public static void main(String[] args) {
+	
+  int n1=10;
+  int n2=5;
+  
+    int result= n1+n2;
+  System.out.println(result);
+  
+    float re=n1-n2;
+  System.out.println(re);
+  
+    double r1=n1*n2;
+  System.out.println(r1);
+  
+   int rf=(n1/n2);
+  System.out.println(rf);
+  
+}
+	
+}
